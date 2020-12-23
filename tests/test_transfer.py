d91412b7b526fee5b55bfbc8e6f8e759757f2cb2
itaven <kurn_97@mail.ru>
2020-12-23 17:18:44 +0300

    test: erc20 spec

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -1,84 +1,85 @@
 import brownie
 import pytest
 
+
 @pytest.fixture(autouse=True)
-def before_each(vault, whale):
-    whale.transfer(vault, "10 ether")
+def before_each(accounts, vault):
+    accounts[0].transfer(vault, "1 ether")
 
 
-def test_sender_balance_decreases(vault, lido, ape, whale):
-    whale_balance_before = vault.balanceOf(whale)
-    amount = "1 ether"
+def test_sender_balance_decreases(accounts, vault):
+    sender_balance = vault.balanceOf(accounts[0])
+    amount = sender_balance // 4
 
-    vault.transfer(ape, amount, {'from': whale})
+    vault.transfer(accounts[1], amount, {'from': accounts[0]})
 
-    assert vault.balanceOf(whale) == whale_balance_before - amount
-
-
-def test_receiver_balance_increases(vault, lido, ape, whale):
-    amount = "1 ether"
-    vault.transfer(ape, "1 ether", {'from': whale})
-
-    ape_balance_before = vault.balanceOf(ape)
-    vault.transfer(ape, amount, {'from': whale})
-
-    assert vault.balanceOf(ape) == ape_balance_before + amount
+    assert vault.balanceOf(accounts[0]) == sender_balance - amount
 
 
-def test_total_supply_not_affected(vault, lido, ape, whale):
-    amount = "1 ether"
+def test_receiver_balance_increases(accounts, vault):
+    receiver_balance = vault.balanceOf(accounts[1])
+    amount = vault.balanceOf(accounts[0]) // 4
 
+    vault.transfer(accounts[1], amount, {'from': accounts[0]})
+
+    assert vault.balanceOf(accounts[1]) == receiver_balance + amount
+
+
+def test_total_supply_not_affected(accounts, vault):
     total_supply = vault.totalSupply()
-    vault.transfer(ape, "1 ether", {'from': whale})
+    amount = vault.balanceOf(accounts[0])
+
+    vault.transfer(accounts[1], amount, {'from': accounts[0]})
 
     assert vault.totalSupply() == total_supply
 
 
-def test_returns_true(vault, lido, ape, whale):
-    amount = "1 ether"
-    vault.transfer(ape, amount, {'from': whale})
-
-    tx = vault.transfer(ape, amount, {'from': whale})
+def test_returns_true(accounts, vault):
+    amount = vault.balanceOf(accounts[0])
+    tx = vault.transfer(accounts[1], amount, {'from': accounts[0]})
 
     assert tx.return_value is True
 
 
-def test_insufficient_balance(vault, lido, ape, whale):
+def test_transfer_full_balance(accounts, vault):
+    amount = vault.balanceOf(accounts[0])
+    receiver_balance = vault.balanceOf(accounts[1])
+
+    vault.transfer(accounts[1], amount, {'from': accounts[0]})
+
+    assert vault.balanceOf(accounts[0]) == 0
+    assert vault.balanceOf(accounts[1]) == receiver_balance + amount
+
+
+def test_transfer_zero_lidos(accounts, vault):
+    sender_balance = vault.balanceOf(accounts[0])
+    receiver_balance = vault.balanceOf(accounts[1])
+
+    vault.transfer(accounts[1], 0, {'from': accounts[0]})
+
+    assert vault.balanceOf(accounts[0]) == sender_balance
+    assert vault.balanceOf(accounts[1]) == receiver_balance
+
+
+def test_transfer_to_self(accounts, vault):
+    sender_balance = vault.balanceOf(accounts[0])
+    amount = sender_balance // 4
+
+    vault.transfer(accounts[0], amount, {'from': accounts[0]})
+
+    assert vault.balanceOf(accounts[0]) == sender_balance
+
+
+def test_insufficient_balance(accounts, vault):
+    balance = vault.balanceOf(accounts[0])
+
     with brownie.reverts():
-        vault.transfer(ape, "100 ether", {'from': whale})
+        vault.transfer(accounts[1], balance + 1, {'from': accounts[0]})
 
 
-def test_transfer_full_balance(vault, lido, ape, whale):
-    amount = vault.balanceOf(whale)
-    ape_balance = vault.balanceOf(ape)
-
-    vault.transfer(ape, amount, {'from': whale})
-
-    assert vault.balanceOf(whale) == 0
-    assert vault.balanceOf(ape) == ape_balance + amount
-
-
-def test_transfer_zero_vaults(vault, lido, ape, whale):
-    sender_balance = vault.balanceOf(whale)
-    receiver_balance = vault.balanceOf(ape)
-
-    vault.transfer(ape, 0, {'from': whale})
-
-    assert vault.balanceOf(whale) == sender_balance
-    assert vault.balanceOf(ape) == receiver_balance
-
-
-def test_transfer_to_self(vault, lido, ape, whale):
-    sender_balance = vault.balanceOf(whale)
-
-    vault.transfer(whale, "1 ether", {'from': whale})
-
-    assert vault.balanceOf(whale) == sender_balance
-
-
-def test_transfer_event_fires(vault, lido, ape, whale):
-    amount = vault.balanceOf(whale)
-    tx = vault.transfer(ape, amount, {'from': whale})
+def test_transfer_event_fires(accounts, vault):
+    amount = vault.balanceOf(accounts[0])
+    tx = vault.transfer(accounts[1], amount, {'from': accounts[0]})
 
     assert len(tx.events) == 1
-    assert tx.events["Transfer"].values() == [whale, ape, amount]
+    assert tx.events["Transfer"].values() == [accounts[0], accounts[1], amount]
