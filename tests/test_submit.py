837a9881515ed483638f9302b49bbdb64b8b5689
Sam Kozin <sam.kozin@gmail.com>
2020-12-23 17:19:09 +0300

    test: submit and withdraw at the same share price

--- tests/test_submit.py
+++ tests/test_submit.py
@@ -1,11 +1,25 @@
-def test_ape_in(vault, lido, ape, helpers):
+def test_ape_in_same_share_price(vault, lido, ape, helpers):
+    ape.transfer(vault, "1 ether")
+    assert lido.balanceOf(ape) == 0
+    assert vault.balanceOf(ape) > 0
+    vault_sheth_balance_before_withdraw = lido.balanceOf(vault)
+    vault_shares_before_withdraw = lido.sharesOf(vault)
+    assert vault.balanceOf(ape) == vault_shares_before_withdraw
+    vault.withdraw({"from": ape})
+    assert lido.balanceOf(ape) == vault_sheth_balance_before_withdraw
+    assert lido.balanceOf(vault) == 0
+    assert vault.balanceOf(ape) == 0
+
+
+def test_ape_in_diff_share_price(vault, lido, ape, helpers):
     ape.transfer(vault, "1 ether")
     assert lido.balanceOf(ape) == 0
     assert vault.balanceOf(ape) > 0
     vault_shares_before_withdraw = lido.sharesOf(vault)
     assert vault.balanceOf(ape) == vault_shares_before_withdraw
     helpers.report_beacon_balance_increase(lido)
-    vault.withdraw()
+    assert vault.balanceOf(ape) == vault_shares_before_withdraw
+    vault.withdraw({"from": ape})
     assert lido.balanceOf(ape) > 0
     assert lido.sharesOf(vault) <= 1 # dust due to rounding error
     assert lido.sharesOf(ape) + lido.sharesOf(vault) == vault_shares_before_withdraw
